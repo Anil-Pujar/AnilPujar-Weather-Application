d4a12c6641d44c285a5267993b60f8bc8a245d4d
Anil <anilpujar5595@gmail.com>
2020-12-18 17:06:04 +0530

    Removed the error

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ const messageTwo = document.querySelector('#message-2')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const Location = search.valuehdhd
+    const Location = search.value
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
